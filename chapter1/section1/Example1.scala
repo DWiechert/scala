87d9aa52a9d817887f61899cf0c5f6079c559efb
Dan Wiechert <dan.wiechert@gmail.com>
2013-07-12 18:03:10 -0500

    Got first example working.

--- chapter1/section1/Example1.scala
+++ chapter1/section1/Example1.scala
@@ -1,4 +1,6 @@
-class Example1 {
+package chapter1.section1
+
+object Example1 extends App {
   var capital = Map("US" -> "Washington", "France" -> "Paris")
   capital += ("Japan" -> "Tokyo")
   println(capital("France"))
