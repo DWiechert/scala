5f527a37d0dd63d5cc3c6f0c6cb5466afb68ecb7
Dan Wiechert <dan.wiechert@gmail.com>
2013-07-26 18:02:47 -0500

    Changed to scripts.

--- chapter2/section5/Example1.scala
+++ chapter2/section5/Example1.scala
@@ -1,9 +1,5 @@
-package chapter2.section5
-
-object Example1 extends App {
-	var i = 0
-	while (i < args.length) {
-	  println(args(i))
-	  i += 1
-	}
+var i = 0
+while (i < args.length) {
+  println(args(i))
+  i += 1
 }
--- chapter2/section5/Example2.scala
+++ chapter2/section5/Example2.scala
@@ -1,13 +1,9 @@
-package chapter2.section5
-
-object Example2 extends App {
-	var i = 0
-	while (i < args.length) {
-	  if (i != 0) {
-	    print(" ")
-	  }
-	  print(args(i))
-	  i += 1
-	}
-	println()
+var i = 0
+while (i < args.length) {
+  if (i != 0) {
+    print(" ")
+  }
+  print(args(i))
+  i += 1
 }
+println()
