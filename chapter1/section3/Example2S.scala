228496761564c137e5663710c52d89c3e6f14feb
Dan Wiechert <dan.wiechert@gmail.com>
2013-07-26 18:01:17 -0500

    Changed to scripts.

--- chapter1/section3/Example2S.scala
+++ chapter1/section3/Example2S.scala
@@ -1,7 +1,3 @@
-package chapter1.section3
-
-object Example2S extends App {
-	val word = "somethingthathasAnuppercase"
-	val wordHasUpperCase = word.exists(_.isUpper)
-	println(wordHasUpperCase)
-}
+val word = "somethingthathasAnuppercase"
+val wordHasUpperCase = word.exists(_.isUpper)
+println(wordHasUpperCase)
