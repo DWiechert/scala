12157f9bc00643bf9c7c9b3b00c66f2c9f97f678
Dan Wiechert <dan.wiechert@gmail.com>
2013-07-26 18:00:23 -0500

    Changed to scripts.

--- chapter1/section1/Example2.scala
+++ chapter1/section1/Example2.scala
@@ -1,7 +1,2 @@
-package chapter1.section1
-
-object Example2 extends App {
-  def factorial(x: BigInt): BigInt = if (x == 0) 1 else x * factorial(x - 1)
-
-  println(factorial(30))
-}
+def factorial(x: BigInt): BigInt = if (x == 0) 1 else x * factorial(x - 1)
+println(factorial(30))
