5a4917107674a944b90ded24e2816d79f14edca3
Dan Wiechert <dan.wiechert@gmail.com>
2013-08-08 18:20:00 -0500

    Added some comments.

--- chapter3/step12/Example2.scala
+++ chapter3/step12/Example2.scala
@@ -5,6 +5,13 @@ def widthOfLength(s: String) = s.length.toString.length
 if (args.length > 0) {
   val lines = Source.fromFile(args(0)).getLines().toList
 
+  /* OO way of thinking 
+   * var maxWidth = 0
+   * for (line <- liens)
+   * maxWidth = maxWidth.max(widthOfLength(line))
+   */
+
+  // Functional way of thinking
   val longestLine = lines.reduceLeft(
     (a, b) => if (a.length > b.length) a else b)
 
