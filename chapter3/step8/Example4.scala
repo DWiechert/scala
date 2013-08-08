5856025212486f4c61ed6267b3904425b38f94d3
Dan Wiechert <dan.wiechert@gmail.com>
2013-08-08 17:42:00 -0500

    Done with chapter 3 step 8.

---
+++ chapter3/step8/Example4.scala
@@ -0,0 +1,32 @@
+List()
+Nil
+List("Cool", "tools", "rule")
+
+val thrill = "Will" :: "fill" :: "until" :: Nil
+
+List("a", "b") ::: List("c", "d")
+
+println(thrill(2))
+println(thrill.count(s => s.length == 4))
+println(thrill.drop(2))
+println(thrill.dropRight(2))
+println(thrill.exists(s => s == "until"))
+println(thrill.filter(s => s.length == 4))
+println(thrill.forall(s => s.endsWith("l")))
+thrill.foreach(s => print(s))
+thrill.foreach(print)
+println(thrill.head)
+println(thrill.init)
+println(thrill.isEmpty)
+println(thrill.last)
+println(thrill.length)
+println(thrill.map(s => s + "y"))
+println(thrill.mkString(", "))
+// Deprecated and removed: println(thrill.remove(s => s.length == 4))
+println(thrill.reverse)
+/* Deprecated and removed: 
+ * println(thrill.sort((s, t) =>
+ *                       s.charAt(0).toLower <
+ *                         t.charAt(0).toLower))
+ */
+println(thrill.tail)
