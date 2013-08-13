5bda72d71627adc6f452c928c7e4f6e64976df74
Dan Wiechert <dan.wiechert@gmail.com>
2013-08-13 18:25:08 -0500

    Done with chapter 5 section 3.
    Done with chapter 5.

---
+++ chapter5/section3/Example1.scala
@@ -0,0 +1,19 @@
+// Adding ints
+println(1 + 2) // Operator notation
+println((1).+(2))
+
+// Adding ints and longs
+println(1 + 2L) // Operator notation
+println((1).+(2L))
+
+// Strings
+val s = "Hello, world!"
+println(s.indexOf('o'))
+println(s indexOf 'o') // Operator notation
+println(s.indexOf('o', 5))
+println(s indexOf ('o', 5)) // Operator notation
+
+// Operator notation
+println(1 + 2) // Infix
+println(-7) // Prefix can only be +, -, !, and ~
+println(7 toLong) // Postfix
