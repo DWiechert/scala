b44a6eb2bdc7857b983f3db6e9c2871f28cf2b49
Dan Wiechert <dan.wiechert@gmail.com>
2013-08-20 18:51:30 -0500

    Done with chapter 6 section 9.

--- chapter6/Rational.scala
+++ chapter6/Rational.scala
@@ -16,10 +16,13 @@ class Rational(n: Int, d: Int) {
 
   override def toString = numer + "/" + denom
 
-  def add(that: Rational): Rational =
+  def +(that: Rational): Rational =
     new Rational(
       numer * that.denom + that.numer * denom,
       denom * that.denom)
+
+  def *(that: Rational): Rational =
+    new Rational(numer * that.numer, denom * that.denom)
 
   def lessThan(that: Rational) =
     numer * that.denom < that.numer * denom
