9fd22e496abae26524a1c0c1d4fcaebe0e08465c
Dan Wiechert <dan.wiechert@gmail.com>
2013-08-20 18:36:31 -0500

    Done with chapter 6 section 5.

--- chapter6/Rational.scala
+++ chapter6/Rational.scala
@@ -4,5 +4,15 @@ package chapter6
  * This class was constructed over all sections of chapter 6.
  */
 class Rational(n: Int, d: Int) {
+  require(d != 0)
+
+  val numer = n
+  val denom = d
+
   override def toString = n + "/" + d
+
+  def add(that: Rational): Rational =
+    new Rational(
+      numer * that.denom + that.numer * denom,
+      denom * that.denom)
 }
