ac6921b43fda7e472458f73d2f5501b8ebfff816
Dan Wiechert <dan.wiechert@gmail.com>
2013-08-20 18:25:35 -0500

    Done with chapter 6 section 3.

--- chapter6/Rational.scala
+++ chapter6/Rational.scala
@@ -4,5 +4,5 @@ package chapter6
  * This class was constructed over all sections of chapter 6.
  */
 class Rational(n: Int, d: Int) {
-  println("Created " + n + "/" + d)
+  override def toString = n + "/" + d
 }
