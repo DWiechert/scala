06ec948d127a4d52e37afcd75cfaf22e3875376b
Dan Wiechert <dan.wiechert@gmail.com>
2013-11-14 18:48:25 -0600

    Done with chapter 7 section 5.

---
+++ chapter7/section5/Example1.scala
@@ -0,0 +1,19 @@
+val firstArg = if (args.length > 0) args(0) else ""
+
+// Directly printing on the match
+firstArg match {
+  case "salt" => println("pepper")
+  case "chips" => println("salsa")
+  case "eggs" => println("bacon")
+  case _ => println("huh?")
+}
+
+// Yielding a result from the match
+val friend =
+  firstArg match {
+    case "salt" => "pepper"
+    case "chips" => "salsa"
+    case "eggs" => "bacon"
+    case _ => "huh?"
+  }
+println(friend)
