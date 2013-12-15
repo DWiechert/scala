b23cbcbf8fa54a4e034193a629d43473cece60f0
Dan Wiechert <dan.wiechert@gmail.com>
2013-12-15 08:42:23 -0600

    Updated example.

--- chapter8/section3/Example2.scala
+++ chapter8/section3/Example2.scala
@@ -1,4 +1,4 @@
 val someNumbers = List(-11, -10, -5, 0, 5, 10)
 someNumbers.foreach((x: Int) => println(x))
 
-println(someNumbers.filter((x: Int) => x > 0))
+println(someNumbers.filter(x => x > 0))
