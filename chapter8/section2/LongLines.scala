a010c6061722a9cd657fc814397a7d69f359b887
Dan Wiechert <dan.wiechert@gmail.com>
2013-12-15 08:25:04 -0600

    Done with chapter 8 section 2.

---
+++ chapter8/section2/LongLines.scala
@@ -0,0 +1,18 @@
+package chapter8.section2
+
+import scala.io.Source
+
+object LongLines {
+  def processFile(filename: String, width: Int) {
+    def processLine(line: String) {
+      if (line.length() > width) {
+        println(filename + ": " + line)
+      }
+    }
+
+    val source = Source.fromFile(filename)
+    for (line <- source.getLines) {
+      processLine(line)
+    }
+  }
+}
