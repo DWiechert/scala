9103fc4baeb8ed249e814df8264c302530d62bb4
Dan Wiechert <dan.wiechert@gmail.com>
2014-08-03 08:30:31 -0500

    Finished chapter 12 section 4.

---
+++ chapter12/section4/Rational.scala
@@ -0,0 +1,62 @@
+package chapter12.section4
+
+/**
+ * This class was constructed over all sections of chapter 6.
+ */
+class Rational(n: Int, d: Int) extends Ordered[Rational] {
+  def compare(that: Rational) = (this.numer * that.denom) - (that.numer * this.denom)
+
+  // All of this code is from chapter 6
+  require(d != 0)
+
+  private val g = gcd(n.abs, d.abs)
+  val numer = n / g
+  val denom = d / g
+
+  // All auxiliary constructors must call either the primary constructor
+  // or another auxiliary constructor which eventually calls the primary
+  def this(n: Int) = this(n, 1) // auxiliary constructor
+
+  override def toString = numer + "/" + denom
+
+  def +(that: Rational): Rational =
+    new Rational(
+      numer * that.denom + that.numer * denom,
+      denom * that.denom)
+
+  def +(i: Int): Rational =
+    new Rational(numer + i * denom, denom)
+
+  def -(that: Rational): Rational =
+    new Rational(
+      numer * that.denom - that.numer * denom,
+      denom * that.denom)
+
+  def -(i: Int): Rational =
+    new Rational(
+      numer - i * denom, denom)
+
+  def *(that: Rational): Rational =
+    new Rational(numer * that.numer, denom * that.denom)
+
+  def *(i: Int): Rational =
+    new Rational(numer * i, denom)
+
+  def /(that: Rational): Rational =
+    new Rational(numer * that.denom, denom * that.numer)
+
+  def /(i: Int): Rational =
+    new Rational(numer, denom * i)
+
+  def lessThan(that: Rational) =
+    numer * that.denom < that.numer * denom
+
+  def max(that: Rational) =
+    if (lessThan(that)) that else this
+
+  private def gcd(a: Int, b: Int): Int =
+    if (b == 0) a else gcd(b, a % b)
+
+  // If you want to do 2 * r, input this function in the REPL:
+  // implicit def intToRational(x: Int) = new Rational(x)
+}
