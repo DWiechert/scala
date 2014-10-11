b0aaf4ebf96f04cddb1a411a790e5c7b695123a6
dwiechert <dan.wiechert@gmail.com>
2014-10-10 19:08:25 -0500

    Done with chapter 14 section 5.

---
+++ chapter14/section5/ElementSpec.scala
@@ -0,0 +1,29 @@
+package chapter14.section5
+
+import org.scalatest.FlatSpec
+import org.scalatest.matchers.ShouldMatchers
+import chapter14.section1.Element.elem
+
+/**
+ * Need to add scala test jar to classpath.
+ * <p/>
+ * Run with: (new ElementSpec).execute()
+ */
+class ElementSpec extends FlatSpec with ShouldMatchers {
+  "A Uniform Element" should
+    "have a width equal to the passed value" in {
+    val ele = elem('x', 2, 3)
+    ele.width should be (2)
+  }
+  
+  it should "have a height equal to the passed value" in {
+    val ele = elem('x', 2, 3)
+    ele.height should be (3)
+  }
+  
+  it should "throw an IAE if passed a negative width" in {
+    evaluating {
+      elem('x', -2, 3)
+    } should produce [IllegalArgumentException]
+  }
+}
