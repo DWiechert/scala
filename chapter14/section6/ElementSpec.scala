6cb568bd48113f9ee04428414430029ce189ad73
dwiechert <dan.wiechert@gmail.com>
2014-10-10 19:15:19 -0500

    Done with chapter 14 section 6. Done with chapter 14.

---
+++ chapter14/section6/ElementSpec.scala
@@ -0,0 +1,28 @@
+package chapter14.section6
+
+import org.scalatest.WordSpec
+import org.scalatest.prop.Checkers
+import org.scalacheck.Prop._
+import chapter14.section1.Element.elem
+
+/**
+ * Need to add scala test jar to classpath.
+ * <p/>
+ * Run with: (new ElementSpec).execute()
+ */
+class ElementSpec extends WordSpec with Checkers {
+  // ScalaCheck will generate hundreds of w's and h's for these checks to see if there are any scenarios
+  // that these properties do not hold true.
+  
+  "elem result" must {
+    "have passed width" in {
+      // For each w > 0, the passed in w must equal the width
+      check((w: Int) => w > 0 ==> (elem('x', w, 3).width == w))
+    }
+    
+    "have passed height" in {
+      // For each h > 0, the passed in h must equal the height
+      check((h: Int) => h > 0 ==> (elem('x', 2, h).height == h))
+    }
+  }
+}
