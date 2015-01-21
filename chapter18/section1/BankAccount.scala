14dbc1a35de19133e1a142583fc7c8c7118147b0
dwiechert <dan.wiechert@gmail.com>
2015-01-21 17:01:39 -0600

    Done with chapter 18 section 1.

---
+++ chapter18/section1/BankAccount.scala
@@ -0,0 +1,19 @@
+package chapter18.section1
+
+class BankAccount {
+  private var bal: Int = 0
+  
+  def balance: Int = bal
+  
+  def deposit(amount: Int) {
+    require(amount > 0)
+    bal += amount
+  }
+  
+  def withdraw(amount: Int): Boolean =
+    if (amount > bal) false
+    else {
+      bal -= amount
+      true
+    }
+}
